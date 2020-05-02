1f0e58ef5995d3e4d703d4c96ae48756bdeb159c
Nathan Hoche <nhoche@desktop-qfru3kp.home>
2020-05-02 16:28:09 +0200

    [ADD]: final map

--- player/player_collision.c
+++ player/player_collision.c
@@ -10,7 +10,7 @@
 
 static int is_collide(char value)
 {
-    char *collide_list = " 1234:=>?@B\0";
+    char *collide_list = " 1234:=>?@BCDFGH\0";
 
     for (int i = 0; collide_list[i]; i++) {
         if (value == collide_list[i])
@@ -19,21 +19,21 @@ static int is_collide(char value)
     return (0);
 }
 
-int player_collision_core(int key, player_t *playr, game_menu_t *game)
+int player_collision_core(int key, player_t *player, game_menu_t *game)
 {
     char **map = game->game_scene.map_layer01;
 
     if (key == sfKeyD && is_collide \
-    (map[playr->pos_cart.y][playr->pos_cart.x + 1]) == 1)
+    (map[player->pos_cart.y][player->pos_cart.x + 1]) == 1)
         return (1);
     else if (key == sfKeyQ && is_collide \
-    (map[playr->pos_cart.y][playr->pos_cart.x - 1]) == 1)
+    (map[player->pos_cart.y][player->pos_cart.x - 1]) == 1)
         return (1);
     if (key == sfKeyS && is_collide \
-    (map[playr->pos_cart.y + 1][playr->pos_cart.x]) == 1)
+    (map[player->pos_cart.y + 1][player->pos_cart.x]) == 1)
         return (1);
     else if (key == sfKeyZ && is_collide \
-    (map[playr->pos_cart.y - 1][playr->pos_cart.x]) == 1)
+    (map[player->pos_cart.y - 1][player->pos_cart.x]) == 1)
         return (1);
     return (0);
 }
