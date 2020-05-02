5031593095c0d8fcc9ea146628a9191800ceed0c
Nathan Hoche <nhoche@desktop-qfru3kp.home>
2020-05-02 14:13:31 +0200

    [FIX]: str_to_word_array

--- lib/my/my_str_to_word_array.c
+++ lib/my/my_str_to_word_array.c
@@ -12,19 +12,20 @@ char **my_str_to_word_array(char *str, char separate)
     char **str_result = NULL;
     int nbr_seperation = 0;
     int y = 0;
+    int j = 0;
 
     for (int i = 0; str[i] != '\0'; i++)
         if (str[i] == separate)
             nbr_seperation++;
-    str_result = malloc(sizeof(char *)*(nbr_seperation+2));
-    for (int pos = 0, x = 0; str[pos] != '\0'; x++, y++) {
+    str_result = malloc(sizeof(char *) * (nbr_seperation + 2));
+    for (int pos = 0, x = 0; str[pos] != '\0'; x++, y++, j = 0) {
         for (; str[y] != '\0' && str[y] != separate; y++);
-        str_result[x] = malloc(sizeof(char) * y-pos);
-        for (int j = 0; pos != y; pos++, j++) {
+        str_result[x] = malloc(sizeof(char) * (y - pos + 1));
+        for (; pos != y; pos++, j++)
             str_result[x][j] = str[pos];
-        }
+        str_result[x][j] = '\0';
         pos = pos + 1;
     }
-    str_result[nbr_seperation+1] = NULL;
+    str_result[nbr_seperation + 1] = NULL;
     return (str_result);
 }
