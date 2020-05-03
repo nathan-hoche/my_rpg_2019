87138ceb6887bee7ba24f0ed5bb62ad1ea7e9c02
Nathan Hoche <nhoche@desktop-qfru3kp.home>
2020-05-03 16:48:31 +0200

    [ADD]: title window

--- main.c
+++ main.c
@@ -44,6 +44,7 @@ static void main_initialization(csfml_t *page)
     (page->icon.icon_img);
     sfRenderWindow_setIcon(page->window, 128, 128, \
     page->icon.icon_uint);
+    sfRenderWindow_setTitle(page->window, "Lost Rpg");
     init_player(&page->player);
     init_music(&page->music);
 }
